f24c6fa71435f7c58bbc003269af6f6919987702
Margaret Nottingham <peg@themnigroup.com>
2021-04-13 08:43:08 -0400

    set custom classes on app component parts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { MainContentService } from 'src/app/services/main-content.service';
 @Component({
 	selector: 'app-page-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
 
 })
-export class HomePageComponent  {}
+export class HomePageComponent implements OnInit {
+	constructor(
+		private main: MainContentService
+	) {
+		let i = this.main.siteHeaderClasses.indexOf('theme-purple');
+		if (i >= 0) {
+			this.main.siteHeaderClasses.splice(i, 1);
+		}
+		this.main.siteMainClasses.push('neg-mt-75');
+		this.main.siteMainClasses.push('position-relative');
+		this.main.siteMainClasses.push('zIndex-1');
+		this.main.siteHeaderClasses.push('position-relative');
+		this.main.siteHeaderClasses.push('zIndex-20');
+	}
+	ngOnInit() {
+
+	}
+}
